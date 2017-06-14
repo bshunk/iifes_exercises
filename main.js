0d5b9146c22b26281d0460c3b023e1642d133087
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-06-14 09:32:53 -0500

    work on iffes exercise

---
+++ main.js
@@ -0,0 +1,44 @@
+// turn this function expression and execution into a single line using the iife pattern
+
+let makeProfile = (function() {
+  const name = "Fred Jones";
+  const age = 37;
+  const likes = ["puppies", "flowers", "pro wrestling"];
+  return {
+    name, age, likes
+  }
+}());
+
+// console.log("the results: ", makeProfile);
+	
+let otherProfile = (function(items) {
+	items.newProp = ["Bob Jones", 38, ["trucks", "beer", "hallmark movies"]];
+	return items
+}(makeProfile));
+
+console.log("the results: ", makeProfile);
+
+
+
+
+// var globalThing = (function() {
+//   let name = "Fred";
+//   return { 
+//     title: "GlobalThing",
+//     printAName: function() {
+//       return name + "Smith";
+//     }
+//   };
+// }());
+
+// var globalThing = (function(thing) {
+//   thing.newProp = "Now I'm Lost";
+//   return thing
+// }(globalThing));
+
+// console.log("global var", globalThing);
+
+
+// Then augment the iife with a second iife that is 
+// configured to accept an argument of an existing 
+// object. (edited)
